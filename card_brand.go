9a6a7ad813f8d51e8e7cebe617fe4ffda77ff495
tranngoclam <tranngoclam288@gmail.com>
2019-09-06 09:37:56 +0700

    Add unit tests

--- card_brand.go
+++ card_brand.go
@@ -1,6 +1,8 @@
 package gocard
 
-import "regexp"
+import (
+	"regexp"
+)
 
 // CardBrand defines card brand properties
 type CardBrand struct {
@@ -22,11 +24,42 @@ const (
 )
 
 // SupportedCardBrands defines a list of supported card brands
+// Reference: https://gist.github.com/michaelkeevildown/9096cd3aac9029c4e6e05588448a8841
 var SupportedCardBrands = map[string]CardBrand{
 	AmericanExpress: {
 		Code:  AmericanExpress,
 		Name:  "American Express",
-		Regex: regexp.MustCompile("^3[47][0-9]{13}$"),
+		Regex: regexp.MustCompile(`^3[47][0-9]{13}$`),
+	},
+	DinersClub: {
+		Code:  DinersClub,
+		Name:  "Diners Club",
+		Regex: regexp.MustCompile(`^3(?:0[0-5]|[68][0-9])[0-9]{11}$`),
+	},
+	Discover: {
+		Code:  Discover,
+		Name:  "Discover",
+		Regex: regexp.MustCompile(`^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$`),
+	},
+	JCB: {
+		Code:  JCB,
+		Name:  "JCB",
+		Regex: regexp.MustCompile(`^(?:2131|1800|35\d{3})\d{11}$`),
+	},
+	MasterCard: {
+		Code:  MasterCard,
+		Name:  "Master Card",
+		Regex: regexp.MustCompile(`^5[1-5][0-9]{14}$`),
+	},
+	UnionPay: {
+		Code:  UnionPay,
+		Name:  "Union Pay",
+		Regex: regexp.MustCompile(`^(62[0-9]{14,17})$`),
+	},
+	Visa: {
+		Code:  Visa,
+		Name:  "Visa",
+		Regex: regexp.MustCompile(`^4[0-9]{12}(?:[0-9]{3})?$`),
 	},
 }
 
@@ -37,13 +70,13 @@ var UnknownCardBrand = CardBrand{
 	Regex: nil,
 }
 
+// Validate checks validity of a card number whether it matches regex or not
+func (brand CardBrand) Validate(cardNumber string) bool {
+	return brand.Regex.MatchString(cardNumber)
+}
+
 // IsCardBrandSupported returns true if card brand is supported
 func IsCardBrandSupported(code string) (supported bool) {
 	_, supported = SupportedCardBrands[code]
 	return supported
-}
-
-// Validate checks validity of a card number whether it matches regex or not
-func (brand CardBrand) Validate(cardNumber string) bool {
-	return brand.Regex.MatchString(cardNumber)
 }
